c4741a4c76a04e0830a4403f88cf3743baf04dd7
Saumya Shastri <saumyashastri@gmail.com>
2019-07-02 16:35:53 -0400

    Fix the "mark as Read" issue
    Fixing the above mentioned issue; added the option to download attachment.

--- Gmail_Access.java
+++ Gmail_Access.java
@@ -10,30 +10,33 @@ import java.util.Properties;
 //enable time frame reads
 public class Gmail_Access
 {
-    String saveDirectory = "Desktop";
-    public static void main (String [] args) throws IOException, MessagingException {
+    String saveDirectory = "Directory path where you want to save any attachments";
+ 
+    public static void main (String [] args) throws IOException, MessagingException
+    {
         Gmail_Access gmail = new Gmail_Access();
         gmail.read();
-
     }
 
-    public void read() throws MessagingException, IOException {
-
+    public void read() throws MessagingException, IOException
+    {
         Properties props = new Properties();
-
         try {
             // Setting protocol properties, and accessing inbox
             props.load(new FileInputStream(new File("src/smtp.properties")));
             Session session = Session.getDefaultInstance(props, null);
             Store store = session.getStore("imaps");
-            store.connect("smtp.gmail.com", "*************@gmail.com", "your_password");
+            String userEmail = "********@gmail.com";
+            String password = getPass.password();
+            store.connect("smtp.gmail.com", userEmail, password);
             Folder inbox = store.getFolder("inbox");
-            inbox.open(Folder.READ_ONLY);
+            inbox.open(Folder.READ_WRITE);
             // Checking for unread emails
             Flags seen = new Flags(Flags.Flag.SEEN);
             FlagTerm unseenFlagTerm = new FlagTerm(seen, false);
             // Checking for unread emails from a particular sender
-            FromTerm senderFromTerm = new FromTerm(new InternetAddress("specifieduser@gmail.com"));
+            String SendingID = "specifieduser@gmail.com";
+            FromTerm senderFromTerm = new FromTerm(new InternetAddress(SendingID));
             Message messages[] = inbox.search(unseenFlagTerm);
             messages = inbox.search(senderFromTerm, messages);
             // if no new message
@@ -41,6 +44,7 @@ public class Gmail_Access
                 System.out.println("No messages found.");
             //found message, now check for attachments
             else
+                System.out.println (messages.length + " unread mail/s from " + SendingID);
                 for(int i = 0; i < messages.length; i++)
                 {
                     Message email = messages[i];
@@ -48,6 +52,7 @@ public class Gmail_Access
                     //contains attachments
                     if (contentType.contains("multipart"))
                     {
+                        System.out.println("Attachments found!");
                         Multipart multipart = (Multipart) email.getContent();
                         int numberOfParts = multipart.getCount();
                         for (int partCount = 0; partCount < numberOfParts; partCount++)
@@ -57,21 +62,27 @@ public class Gmail_Access
                             {
                                 // this part is attachment, saving attachment
                                 String fileName = part.getFileName();
-                                String attachFiles = "";
-                                attachFiles += fileName + ", ";
-                                part.saveFile(saveDirectory + File.separator + fileName);
+                                Scanner scan = new Scanner(System.in);
+                                System.out.println("Do you want to download the found attachment? Enter Y for yes.");
+                                String response = scan.nextLine();
+                                if (response.equals("Y"))
+                                {
+                                    String attachFiles = "";
+                                    attachFiles += fileName + ", ";
+                                    System.out.println("Downloading: " + fileName);
+                                    part.saveFile(saveDirectory + File.separator + fileName);
+                                }
+                                else
+                                    continue;
                             }
                         }
                     }
                     //Marking email read
                     email.setFlag(Flags.Flag.SEEN,true);
                 }
-
             inbox.close(true);
             store.close();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        } 
+        catch (Exception e) { e.printStackTrace(); }
     }
 }
